f3cbeec4c2b5af40182a14512324718cda7c4371
ddrake-geojobe <ddrake@geo-jobe.com>
2023-05-29 20:48:10 -0500

    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
+import { useEffect, useRef } from 'react'
+import Map from '@arcgis/core/Map'
+import MapView from '@arcgis/core/views/MapView'
 
 function App() {
+  const mapDiv = useRef()
+  const map = useRef()
+  const mapView = useRef()
+
+  useEffect(() => {
+    map.current = new Map({
+      basemap: 'streets-vector'
+    });
+    
+    mapView.current = new MapView({
+      map: map.current
+    });
+
+    mapView.current.container = mapDiv.current
+  }, [])
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      ref={mapDiv}
+      style={{
+        height: '100vh',
+        width: '100vw'
+      }}
+    >
     </div>
   );
 }
